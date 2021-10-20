524f69e65add069762723e0e931280752b2a7038
Ruchama3242 <ruchama.simons@gmail.com>
2021-10-20 22:18:02 +0300

    initial code+welcome function

---
+++ Targil0/Program0692.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome0692();
+            welcome3869();
+            Console.ReadKey();
+        }
+        static partial void welcome3869();
+        private static void welcome0692()
+        {
+            Console.WriteLine("Enter yout name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", name);
+        }
+    }
+}
