bfcbe1ca49df7ab95a65fc30495fc58c63ea86f6
noa-ankonina <nankonin@g.jct.ac.il>
2021-11-18 23:37:07 +0200

    nothing

--- BL/BL.cs
+++ BL/BL.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BL.BO;
+using IBL.BO;
 namespace BL
 {
     class BL
     {
+       
         List<IBL.BO.DroneToList> DroneArr;
         public void updateNameDrone(int ID, int model)
         {
@@ -16,6 +18,10 @@ namespace BL
                 flug.droneModel = model;
             else 
                 throw new IdUnExistsException("the ID don't mutch to any drone");
+        }
+
+        public void updateStation(int Id ,string name, int emptyChargeSlot)
+        {
             
         }
     }
