0702c271045156d1108adf7aa59f157fa85d3aef
noa-ankonina <nankonin@g.jct.ac.il>
2021-11-18 16:51:44 +0200

    1

--- BL/BL.cs
+++ BL/BL.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BL.BO;
 namespace BL
 {
     class BL
     {
+        List<IBL.BO.DroneToList> DroneArr;
         public void updateNameDrone(int ID, int model)
         {
-
+            var flug = DroneArr.Find(p => p.ID == ID);
+            if (flug != null)
+                flug.droneModel = model;
+            else 
+                throw new IdUnExistsException("the ID don't mutch to any drone");
+            
         }
     }
 }
