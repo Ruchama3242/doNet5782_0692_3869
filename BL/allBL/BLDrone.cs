fc4fe8ceb0a9a600025d06c83fde789f6812d3c7
Ruchama3242 <ruchama.simons@gmail.com>
2021-11-22 17:59:00 +0200

    exe2

--- BL/allBL/BLDrone.cs
+++ BL/allBL/BLDrone.cs
@@ -38,17 +38,19 @@ namespace BL
                 d.status = IBL.BO.DroneStatus.maintenace;
                 try
                 {
-                    IDAL.DO.Station s = dl.printStation(stationId);
+                    IDAL.DO.Station s = dl.findStation(stationId);
                     d.currentLocation.latitude = s.lattitude;
                     d.currentLocation.longitude = s.longitude;
                 }
-                catch (Exception e)
+                catch (Exception )
                 {
                     throw new BLIdUnExistsException("Error! The station dosen't exist");
                 }
-                IDAL.DO.Drone dr = droneDal(d);
-                dl.addDrone(dr);
-                DroneArr.Add(d);
+                IDAL.DO.Drone dr = droneDal(d);//drone of "DAL" type
+                dl.addDrone(dr);//add drone to the list in the dal
+                DroneArr.Add(d);//add drone to the list of the drones in the BL
+                IDAL.DO.DroneCharge dc = new IDAL.DO.DroneCharge { droneID = id, stationeld = stationId };
+                dl.BatteryCharged(dc);//send the drone to charge
             }
             catch(Exception e)
             {
@@ -82,6 +84,98 @@ namespace BL
 
         }
 
-        public IBL.BO.Drone getBlDrone()
+        public IBL.BO.Drone getBlDrone(int id)
+        {
+            try
+            {
+                var drn = DroneArr.Find(x => x.ID == id);
+                if (drn == null)
+                    throw new BLIdUnExistsException("Error! the drone doesn't found");
+                IBL.BO.Drone d = new IBL.BO.Drone();
+                d.ID = drn.ID;
+                d.model = drn.droneModel;
+                d.weight = drn.weight;
+                d.status = drn.status;
+                d.battery = drn.battery;
+                d.location = drn.currentLocation;
+                IBL.BO.ParcelInTransfer pt = new IBL.BO.ParcelInTransfer();
+                if (drn.status == IBL.BO.DroneStatus.delivery)
+                {
+                    pt.ID = drn.parcelNumber;
+                    IDAL.DO.Parcel p;
+                    try
+                    {
+                        p = dl.printParcel(drn.parcelNumber);//get the parcel from the dal
+                    }
+                    catch (Exception)
+                    {
+                        throw new BLIdUnExistsException("Error! the parcel not found");
+                    }
+                    if (p.pickedUp == DateTime.MinValue)
+                        pt.status = false;
+                    else
+                        pt.status = true;
+                    pt.priority = (IBL.BO.Priorities)p.priority;
+                    pt.weight = (IBL.BO.WeightCategories)p.weight;
+                    pt.sender = getCustomerInParcel(p.senderID);
+                    pt.target = getCustomerInParcel(p.targetId);
+                    IDAL.DO.Customer sender = dl.findCustomer(p.senderID);
+                    IDAL.DO.Customer target = dl.findCustomer(p.targetId);
+                    pt.collectionLocation.longitude = sender.longitude;
+                    pt.collectionLocation.latitude = sender.lattitude;
+                    pt.targetLocation.longitude = target.longitude;
+                    pt.targetLocation.latitude = target.lattitude;
+                    pt.distance = distance(pt.collectionLocation, pt.targetLocation);
+                }
+                d.parcel = pt;
+                return d;
+            }
+            catch (Exception)
+            {
+                throw new BLgeneralException("Error!");
+            }
+        }
+
+        private IBL.BO.CustomerInParcel getCustomerInParcel(int id)
+        {
+            IBL.BO.CustomerInParcel c = new IBL.BO.CustomerInParcel();
+            c.ID = id;
+            IDAL.DO.Customer cs = dl.findCustomer(id);
+            c.customerName = cs.name;
+            return c;
+        }
+        /// <summary>
+        /// calculate a distance between 2 points
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        private double distance(IBL.BO.Location l1,IBL.BO.Location l2)
+        {
+            var R = 6371; // Radius of the earth in km
+            var dLat = deg2rad(l2.latitude - l1.latitude);  // deg2rad below
+            var dLon = deg2rad(l2.longitude - l1.longitude);
+            var a =
+              Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+              Math.Cos(deg2rad(l1.latitude)) * Math.Cos(deg2rad(l2.latitude)) *
+              Math.Sin(dLon / 2) * Math.Sin(dLon / 2)
+              ;
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c; // Distance in km
+            return d;
+        }
+
+        private double deg2rad(double deg)
+        {
+            return deg * (Math.PI / 180);
+        }
+
+        public IEnumerable<IBL.BO.DroneToList> getAllDrones()
+        {
+            List<IBL.BO.DroneToList> lst = new List<IBL.BO.DroneToList>();
+            foreach (var item in DroneArr)
+                lst.Add(item);
+            return lst;
+        }
     }
 }
---
+++ BL/allBL/BLParcel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.BO;
+
+namespace BL
+{
+    partial class BL
+    {
+        public void addParcel(int senderId,int targetId,int weight,int priority)
+        {
+          
+                IDAL.DO.Parcel p = new IDAL.DO.Parcel();
+                p.senderID = senderId;
+                p.targetId = targetId;
+                p.weight = (IDAL.DO.WeightCategories)weight;
+                p.priority = (IDAL.DO.Priorities)priority;
+                p.droneID = 0;
+                p.requested = DateTime.Now;
+                p.pickedUp = DateTime.MinValue;
+                p.scheduled = DateTime.MinValue;
+                p.delivered = DateTime.MinValue;
+                dl.addParcel(p);
+            
+            
+        }
+    }
+}
