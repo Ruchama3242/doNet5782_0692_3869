2d89fb35dbde73025ba8974cc0cd0756cc73f733
noa-ankonina <nankonin@g.jct.ac.il>
2021-11-29 00:25:00 +0200

    bnjb

--- BL/allBL/BLStation.cs
+++ BL/allBL/BLStation.cs
@@ -107,6 +107,16 @@ namespace BL
                 IBL.BO.Station s = new IBL.BO.Station();
                 IDAL.DO.Station sD = myDalObject.findStation(id);
                 s = convertStation(sD);
+                List<IDAL.DO.DroneCharge> drCh = new List<IDAL.DO.DroneCharge>();
+                //drCh = dl.findDroneCharge(id);
+                //s.dronesInChargeList = new List<IBL.BO.DroneInCharge>();
+                //foreach (var item in drCh)
+                //{
+                //    IBL.BO.DroneInCharge tmp = new IBL.BO.DroneInCharge();
+                //    tmp.battery = (findDrone(item.droneID).battery);
+                //    tmp.ID = item.droneID;
+                //    s.dronesInChargeList.Add(tmp);
+                //}
                 return s;
             }
             catch (Exception e)
