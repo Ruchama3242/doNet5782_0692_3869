7d17f4b7253eb72bc46dc2e420db6861fce6ef15
Ruchama3242 <ruchama.simons@gmail.com>
2021-12-05 19:35:16 +0200

    exe3

--- PL/droneView.xaml.cs
+++ PL/droneView.xaml.cs
@@ -20,19 +20,230 @@ namespace PL
     /// </summary>
     public partial class droneView : Window
     {
+        private IBL.interfaceIBL bl;
+        private DroneToList dr;
         public droneView()
         {
             InitializeComponent();
         }
 
-        public droneView(IBL.interfaceIBL bl,string s)
+        public droneView(IBL.interfaceIBL bl)
         {
-            if (s == "add")
-               
             InitializeComponent();
-            ___DroneMaxWeigt.ItemsSource = Enum.GetValues(typeof(WeightCategories));
+            this.bl = bl;
+            updateGrid.Visibility = Visibility.Hidden;
+            
+           DroneMaxWeigtCmb.ItemsSource = Enum.GetValues(typeof(WeightCategories));
+            
         }
-      
-      
+
+        public droneView(IBL.interfaceIBL bl,DroneToList d)
+        {
+            InitializeComponent();
+            this.bl = bl;
+            dr = d;
+            addGrid.Visibility = Visibility.Hidden;
+            fillTextbox(d);
+            if(dr.status==DroneStatus.Available)
+            {
+                droneChargeBtn.Visibility = Visibility.Visible;
+                sendToDeliveryBtn.Visibility = Visibility.Visible;
+            }
+
+            if(dr.status==DroneStatus.Maintenace)
+            {
+                relaseBtn.Visibility = Visibility.Visible;
+            }
+
+            if(dr.status==DroneStatus.Delivery)
+            {
+                collectBtn.Visibility = Visibility.Visible;
+                parcelDeliveryBtn.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void addDroneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.addDrone(Convert.ToInt32(idTxt), Convert.ToInt32(modelTxt), Convert.ToInt32(DroneMaxWeigtCmb.SelectedItem), Convert.ToInt32(idStationTxt));
+                MessageBox.Show("the drone was successfully added");
+                
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+               
+            }
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void updateModel(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.updateNameDrone(dr.ID, Convert.ToInt32(updateModeltxt.Text));
+                MessageBox.Show("the model of the drone was successfully updated");
+
+                Drone drone = bl.findDrone(dr.ID);
+                fillTextbox(drone);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+               
+            }
+        }
+
+        private void fillTextbox(DroneToList d)
+        {
+            
+            statusTxt.Text = d.status.ToString();
+            weightTxt.Text = d.weight.ToString();
+            updateIdtxt.Text = d.ID.ToString();
+            updateModeltxt.Text = d.droneModel.ToString();
+            batteryTxt.Text = d.battery.ToString();
+            parcelIdTxt.Text = d.parcelNumber.ToString();
+            longitudeTxt.Text = d.currentLocation.longitude.ToString();
+            latitudeTxt.Text = d.currentLocation.latitude.ToString();
+        }
+
+        private void fillTextbox(Drone d)
+        {
+            statusTxt.Text = d.status.ToString();
+            updateIdtxt.Text = d.ID.ToString();
+            weightTxt.Text = d.weight.ToString();
+            updateModeltxt.Text = d.model.ToString();
+            batteryTxt.Text = d.battery.ToString();
+            if (d.parcel != null)
+            {
+                parcelIdTxt.Text = d.parcel.ID.ToString();
+
+            }
+            else
+                parcelIdTxt.Text = 0.ToString();
+            longitudeTxt.Text = d.location.longitude.ToString();
+            latitudeTxt.Text = d.location.latitude.ToString();
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sendToCharge(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.sendToCharge(dr.ID);
+                MessageBox.Show("the drone was sent to charge");
+                Drone drone = bl.findDrone(dr.ID);
+                fillTextbox(drone);
+                relaseBtn.Visibility = Visibility.Visible;
+                droneChargeBtn.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+        }
+
+        private void relaseFromCharge(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lbl.Visibility = Visibility.Visible;
+                timeInChargeTxt.Visibility = Visibility.Visible;
+                okBtn.Visibility = Visibility.Visible;
+                MessageBox.Show("enter time(in minuits) that drone was in charging");
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bl.releaseFromCharge(dr.ID, Convert.ToInt32(timeInChargeTxt.Text));
+            MessageBox.Show("the drone was relase from charge");
+            Drone drone = bl.findDrone(dr.ID);
+            fillTextbox(drone);
+            relaseBtn.Visibility = Visibility.Hidden;
+            lbl.Visibility = Visibility.Hidden;
+            timeInChargeTxt.Visibility = Visibility.Hidden;
+            okBtn.Visibility = Visibility.Hidden;
+            droneChargeBtn.Visibility = Visibility.Visible;
+            sendToDeliveryBtn.Visibility = Visibility.Visible;
+        }
+
+        private void sendToDelivery(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.parcelToDrone(dr.ID);
+                MessageBox.Show("the drone belongs to parcel");
+                Drone drone = bl.findDrone(dr.ID);
+                fillTextbox(drone);
+                collectBtn.Visibility = Visibility.Visible;
+                parcelDeliveryBtn.Visibility = Visibility.Visible;
+                droneChargeBtn.Visibility = Visibility.Hidden;
+                sendToDeliveryBtn.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void collectParcel(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.packageCollection(dr.ID);
+                Drone drone = bl.findDrone(dr.ID);
+                fillTextbox(drone);
+                MessageBox.Show("the parcel was collected by the parcel");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+               
+            }
+        }
+
+        private void parcelDelivery(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                bl.packageDelivery(dr.ID);
+                MessageBox.Show("the parcel was delivered to the customer");
+                Drone drone = bl.findDrone(dr.ID);
+                fillTextbox(drone);
+               
+                collectBtn.Visibility = Visibility.Hidden;
+                parcelDeliveryBtn.Visibility = Visibility.Hidden;
+                droneChargeBtn.Visibility = Visibility.Visible;
+                sendToDeliveryBtn.Visibility = Visibility.Visible;
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
