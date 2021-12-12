55732242b02348e31b4a672b568db129ee01f4f3
noa-ankonina <nankonin@g.jct.ac.il>
2021-12-12 19:07:27 +0200

    PR01

--- BL/BlFactory.cs
+++ BL/BlFactory.cs
@@ -9,11 +9,8 @@ namespace BL
    static public class BlFactory
     {
         static public  IBL.IBL GetBl()
-        {
-
-            //BlApi.BL bl = BlApi.BL.Instance;
+        { 
             IBL.IBL bl = BlApi.BL.Instance;
-
             return bl;
         }
     }
