06bcaea76cf226f9a1b47425fb220e1c241e5751
noa-ankonina <nankonin@g.jct.ac.il>
2021-12-21 20:39:36 +0200

    מה אתה רוצה ממני מההההה

--- PL/customerListViewWindow.xaml.cs
+++ PL/customerListViewWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using BO;
 
 namespace PL
@@ -20,28 +22,47 @@ namespace PL
     /// <summary>
     /// Interaction logic for customerListView.xaml
     /// </summary>
+    /// 
+   // private List<DO.Customer> myCollection = new List<DO.Customer>();
     public partial class customerListView : Window
     {
         private BlApi.IBL bl = BlApi.BlFactory.GetBl();
 
-        private ObservableCollection<BO.Customer> _myCollection =
+        private ObservableCollection<BO.Customer> myCollection =
       new ObservableCollection<BO.Customer>();
-
+        
        
         public customerListView()
         {
             InitializeComponent();
+            //this.myCollection = bl.viewListCustomer();
+            DataContext = myCollection;
         }
 
         public customerListView(BlApi.IBL bl)
         {
             InitializeComponent();
+            DataContext = bl.viewListCustomer();
             this.bl = bl;
         }
 
         private void add_Click(object sender, RoutedEventArgs e)
         {
-            new regist(bl).ShowDialog();
+            new regist(bl, myCollection).ShowDialog();
+           
+           // DataContext = this.myCollection;
+          // lstBoxCustomer.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            // myCollection.Add(bl.findCustomer
+        }
+
+        private void ListBox_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+
+        private void lstBoxCustomer_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
         }
     }
 }
