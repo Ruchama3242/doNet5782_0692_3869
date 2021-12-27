b0edfa9453fc55e5bf0da451bb48d6307e123e8f
noa-ankonina <nankonin@g.jct.ac.il>
2021-12-27 19:06:28 +0200

    עידכונים בחלון לקוח

--- BL/BO/CustomerToList.cs
+++ BL/BO/CustomerToList.cs
@@ -5,23 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BO
+{
+    public class CustomerToList
     {
-        public class CustomerToList
+        public int ID;
+        public string name;
+        public string phone;
+        public int sendAndDeliveredParcels;
+        public int sendAndNotDeliveredParcels;
+        public int gotParcels;
+        public int onTheWayParcels;
+        public override string ToString()
         {
-            public int ID;
-            public string name;
-            public string phone;
-            public int sendAndDeliveredParcels;
-            public int sendAndNotDeliveredParcels;
-            public int gotParcels;
-            public int onTheWayParcels;
-            public override string ToString()
-            {
-                String result = "";
-                result += $"ID: {ID}, Name: {name}, phone number: {phone.Substring(0, 3) + '-' + phone.Substring(3)}, {sendAndDeliveredParcels} Packages were successfully delivered" +
-                    $", {sendAndNotDeliveredParcels} Packages sending , {gotParcels} Packages received, {onTheWayParcels} on the way'\n";
-                return result;
-            }
+            String result = "";
+            result += $"ID: {ID}, Name: {name}, phone number: {phone.Substring(0, 3) + '-' + phone.Substring(3)}, {sendAndDeliveredParcels} Packages were successfully delivered" +
+                $", {sendAndNotDeliveredParcels} Packages sending , {gotParcels} Packages received, {onTheWayParcels} on the way'\n";
+            return result;
         }
     }
+}
 
--- PL/addCustomerWindoes.xaml.cs
+++ PL/addCustomerWindoes.xaml.cs
@@ -29,7 +29,27 @@ namespace PL
             c = new Customer();
             c.location = new BO.Location();
             this.DataContext = c;
+            this.updateBtn.Visibility = Visibility.Hidden;
+            this.deleteBtn.Visibility = Visibility.Hidden;
         }
+
+        public addCustomerWindoes( CustomerToList c)
+        {
+            InitializeComponent();
+            bl = BlApi.BlFactory.GetBl();
+           // c = new CustomerToList();
+            this.longitudeL.Visibility = Visibility.Hidden;
+            this.lattitudeL.Visibility = Visibility.Hidden;
+            this.lattitudeBtn.Visibility = Visibility.Hidden;
+            this.longitudeBtn.Visibility = Visibility.Hidden;
+            this.DataContext = c;
+            this.updateBtn.Visibility = Visibility.Visible;
+            this.deleteBtn.Visibility = Visibility.Visible;
+            this.nameBtn.IsReadOnly = true;
+            this.IDBtn.IsReadOnly = true;
+            this.phoneBtn.IsReadOnly = true;
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -51,6 +71,34 @@ namespace PL
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.deleteCustomer(c.ID);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bl.updateCustomer(c.ID, c.name, c.phone);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
         }
     }
 }
